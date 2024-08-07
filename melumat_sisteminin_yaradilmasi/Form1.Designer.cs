99710261ca491f67c61640382a2d0413cde6f84b
SOLO_LEVELING <SOLO_LEVELING@DESKTOP-AFTR4IL>
2024-08-07 20:53:34 +0200

    Add project files.

---
+++ melumat_sisteminin_yaradilmasi/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace melumat_sisteminin_yaradilmasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(715, 275);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(459, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "\"Xidmeti mektublar\" melumat sistemi";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.CustomFormat = "MM - dd - yyyy hh: mm:ss";
+            this.dateTimePicker1.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(905, 214);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(230, 27);
+            this.dateTimePicker1.TabIndex = 26;
+            this.dateTimePicker1.Visible = false;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CalendarFont = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.CustomFormat = "MM - dd - yyyy hh: mm:ss";
+            this.dateTimePicker2.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(905, 264);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(230, 27);
+            this.dateTimePicker2.TabIndex = 28;
+            this.dateTimePicker2.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(975, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 20);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Baslangic tarix";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(988, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 20);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Bitme tarixi";
+            this.label5.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(101, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 38);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "Sil";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(328, 351);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 38);
+            this.button3.TabIndex = 35;
+            this.button3.Text = "Çap";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Movzu adı",
+            "Mektubun kime unvanlanmasi",
+            "Mektubun kim terefinden yazilmasi",
+            "Sobe",
+            "Mektubun baslangic ve bitme tarixine uygun"});
+            this.comboBox1.Location = new System.Drawing.Point(914, 119);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(207, 25);
+            this.comboBox1.TabIndex = 41;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(974, 351);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(112, 38);
+            this.button9.TabIndex = 42;
+            this.button9.Text = "Axtar";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(890, 150);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(257, 33);
+            this.textBox6.TabIndex = 43;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(953, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 44;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(942, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 20);
+            this.label3.TabIndex = 45;
+            this.label3.Text = "Verilenleri daxil edin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1239, 461);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ melumat_sisteminin_yaradilmasi/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace melumat_sisteminin_yaradilmasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connect;
+        SqlCommand com;
+        SqlDataAdapter da;
+
+
+        void example()
+        {
+            connect = new SqlConnection("Data Source=DESKTOP-003N1MJ\\SQLEXPRESS;Initial Catalog=Elchin_diplom;Integrated Security=SSPI");
+            connect.Open();
+            da = new SqlDataAdapter("SELECT *from dbo.mektublar", connect);
+            DataTable cedvel = new DataTable();
+            da.Fill(cedvel);
+            dataGridView1.DataSource = cedvel;
+            connect.Close();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            example();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            textBox6.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox6.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox6.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox6.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+
+            dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            dateTimePicker2.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+
+            textBox6.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            /*
+                string sual = "DELETE FROM dbo.mektublar where ID=@ID";
+                com = new SqlCommand(sual, connect);
+                com.Parameters.AddWithValue("@ID", textBox6.Text);
+                com.Parameters.AddWithValue("@Movzu", textBox6.Text);
+                com.Parameters.AddWithValue("@Kime", textBox6.Text);
+                com.Parameters.AddWithValue("@Kimden", textBox6.Text);
+                com.Parameters.AddWithValue("@Sobeler", textBox6.Text);
+
+                com.Parameters.AddWithValue("@Gonderilme_tarixi", dateTimePicker1.Text);
+                com.Parameters.AddWithValue("@Qebul_tarixi", dateTimePicker2.Text);
+
+
+                connect.Open();
+                com.ExecuteNonQuery();
+                connect.Close();
+                example();
+            */
+            
+
+            
+
+
+        }
+
+
+
+        Bitmap bitmap;
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.PrintPreviewControl.Zoom = 1;
+            printPreviewDialog1.ShowDialog();
+
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+            Bitmap imagebmp = new Bitmap(dataGridView1.Width, dataGridView1.Height);
+            dataGridView1.DrawToBitmap(imagebmp, new Rectangle(0, 0, dataGridView1.Width, dataGridView1.Height));
+            e.Graphics.DrawImage(imagebmp, 60, 20);
+
+        }
+
+
+
+
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            if (comboBox1.Text == "Movzu adı")
+            {
+
+                connect.Open();
+                string search = textBox6.Text;
+
+                try
+                {
+                    SqlCommand comm = new SqlCommand("select * from dbo.mektublar where Movzu=@Movzu", connect);
+                    comm.Parameters.AddWithValue("Movzu", textBox6.Text);
+                    SqlDataAdapter ads = new SqlDataAdapter(comm);
+                    DataTable dt = new DataTable();
+                    ads.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+
+            }
+
+            if (comboBox1.Text == "Mektubun kime unvanlanmasi")
+            {
+
+                connect.Open();
+                string search = textBox6.Text;       //search evezine axtar yazmisam
+
+                try
+                {
+                    SqlCommand comm = new SqlCommand("select * from dbo.mektublar where Kime=@Kime", connect);
+                    comm.Parameters.AddWithValue("Kime", textBox6.Text);
+                    SqlDataAdapter ads = new SqlDataAdapter(comm);
+                    DataTable dt = new DataTable();
+                    ads.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+
+            }
+
+            if (comboBox1.Text == "Mektubun kim terefinden yazilmasi")
+            {
+
+                connect.Open();
+                string look = textBox6.Text; // look axtarmaq ucun istifade olunan bizim verdiyimi bir addı
+
+                try
+                {
+                    SqlCommand comm = new SqlCommand("select * from dbo.mektublar where Kimden=@Kimden", connect);
+                    comm.Parameters.AddWithValue("Kimden", textBox6.Text);
+                    SqlDataAdapter ads = new SqlDataAdapter(comm);
+                    DataTable dt = new DataTable();
+                    ads.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+
+            }
+
+
+            if (comboBox1.Text == "Mektubun baslangic ve bitme tarixine uygun")
+            {
+
+                label4.Visible = true;
+                label5.Visible = true;
+                dateTimePicker1.Visible = true;
+                dateTimePicker2.Visible = true;
+
+
+                SqlConnection con = new SqlConnection("Data Source=DESKTOP-003N1MJ\\SQLEXPRESS;Initial Catalog=Elchin_diplom; Integrated Security = SSPI");
+                DataTable ds = new DataTable();
+                string sql = "SELECT ID,Movzu,Kime,Kimden,Gonderilme_tarixi,Qebul_tarixi FROM dbo.mektublar WHERE Gonderilme_tarixi BETWEEN @tarix1 and @tarix2";
+                SqlDataAdapter ad = new SqlDataAdapter(sql, con);
+                ad.SelectCommand.Parameters.AddWithValue("@tarix1", dateTimePicker1.Value);
+                ad.SelectCommand.Parameters.AddWithValue("@tarix2", dateTimePicker2.Value);
+                con.Open();
+                ad.Fill(ds);
+                dataGridView1.DataSource = ds;
+                con.Close();
+
+            }
+
+            if (comboBox1.Text == "Sobe")
+            {
+
+                connect.Open();
+                string loook = textBox6.Text;
+
+                try
+                {
+                    SqlCommand comm = new SqlCommand("select * from dbo.mektublar where Sobeler=@Sobeler", connect);
+                    comm.Parameters.AddWithValue("Sobeler", textBox6.Text);
+                    SqlDataAdapter ads = new SqlDataAdapter(comm);
+                    DataTable dt = new DataTable();
+                    ads.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+
+            }
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (comboBox1.Text == "Movzu adı")
+            {
+
+                label4.Visible = false;
+                label5.Visible = false;
+                dateTimePicker1.Visible = false;
+                dateTimePicker2.Visible = false;
+
+            }
+
+            if (comboBox1.Text == "Mektubun kime unvanlanmasi")
+            {
+
+                label4.Visible = false;
+                label5.Visible = false;
+                dateTimePicker1.Visible = false;
+                dateTimePicker2.Visible = false;
+
+            }
+
+            if (comboBox1.Text == "Mektubun kim terefinden yazilmasi")
+            {
+
+                label4.Visible = false;
+                label5.Visible = false;
+                dateTimePicker1.Visible = false;
+                dateTimePicker2.Visible = false;
+
+            }
+
+
+            if (comboBox1.Text == "Mektubun baslangic ve bitme tarixine uygun")
+            {
+
+                label4.Visible = true;
+                label5.Visible = true;
+                dateTimePicker1.Visible = true;
+                dateTimePicker2.Visible = true;
+
+
+
+            }
+
+            if (comboBox1.Text == "Sobe")
+            {
+
+                label4.Visible = false;
+                label5.Visible = false;
+                dateTimePicker1.Visible = false;
+                dateTimePicker2.Visible = false;
+
+            }
+        }
+    }
+}
